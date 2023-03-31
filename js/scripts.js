bc8473dca6a959ab9f7254871ef6224d11f5e460
Cristobal <39746703+sauta@users.noreply.github.com>
2023-03-30 22:35:37 -0300

    aplcaindo una integracion con laravel

--- js/scripts.js
+++ js/scripts.js
@@ -18,53 +18,7 @@
              var imagenNombre = $("#newImageName").val();
             console.log("Nuevo nombre de imagen:", imagenNombre);
 
-
-            let formData = new FormData(this);
-            $.ajax({
-                url: "upload.php",
-                type: "POST",
-                data: formData,
-                processData: false,
-                contentType: false,
-                beforeSend: function() {
-                  
-                    
-                },
-                success: function(response) {
-                    showMessage(response);
-                
-                    let inputImagen = $("#input-imagen")[0];
-                        let reader = new FileReader();
-                        reader.onload = function(e) {
-                            let imagenURL = e.target.result;
-                            let imagenExtension = inputImagen.files[0].name.split('.').pop();
-                            let imagenNombre = nuevoNombreImagen + '.' + imagenExtension; // Usa nuevoNombreImagen
-                            $("#imagen-giratoria").attr("src", imagenURL).show();
-                            $("#boton-girar").show();
-
-                            // Agrega la tarjeta con la imagen cargada y su nombre
-                            let cardHTML = `
-                                <div class="col-md-4 mt-4">
-                                    <div class="card">
-                                        <img src="${imagenURL}" class="card-img-top" alt="${imagenNombre}">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${imagenNombre}</h5>
-                                            <p class="card-text">Esta es una imagen cargada por el usuario.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            `;
-                            $("#imagenes-contenedor").append(cardHTML);
-                        };
-                    reader.readAsDataURL(inputImagen.files[0]);
-                
-                    // Elimina el campo oculto después de enviar el formulario
-                    $("#newImageName").remove();
-                },
-                error: function() {
-                    showMessage("Error al subir el archivo.");
-                }
-            });
+            uploadImage()
         });
 
         $("#boton-girar").on("click", function() {
@@ -113,3 +67,24 @@
         }
         
     });
+
+    function uploadImage() {
+        var formData = new FormData();
+        formData.append("image", fileInput.files[0]);
+    
+        $.ajax({
+            url: "http://localhost:8000/api/images",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                $('#uploadSuccessModal').modal('show');
+                $('.card-img-top').attr('src', 'http://localhost:8000/uploads/' + response.filename);
+                $('.card-title').text(response.filename);
+            },
+            error: function () {
+                $('#uploadErrorModal').modal('show');
+            },
+        });
+    }
